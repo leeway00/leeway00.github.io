268938b597123501679e900532254b0741eb5933
leeway00 <leeway00@users.noreply.github.com>
2022-04-25 02:56:04 +0000

    deploy: 867713cb9ff5f8d73f769735df7126f3e898546d

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Quick-Start Guide",
+        "excerpt":"Minimal Mistakes has been developed as a Gem-based theme for easier use, and 100% compatible with GitHub Pages when used as a remote theme. If you enjoy this theme, please consider sponsoring me to continue developing and maintaining it. Installing the theme If you’re running Jekyll v3.7+ and self-hosting you...","categories": [],
+        "tags": [],
+        "url": "/data_science/test/",
+        "teaser": null
+      },{
+        "title": "Chapter3 Special Distributions",
+        "excerpt":"Bernoulli and Binomial Bernoulli Distribution Bernoulli experiment a random experiment that outcome are classified with two mutually exclusive and exhaustive ways Bernoulli process a sequence of Bernoulli trials. Let X be a random variable associated with a Bernoulli trial The pmf of X is p(x)=px(1−p)1−x,x=0,1p(x) = p^x(1-p)^{1-x}, x=0,1p(x)=px(1−p)1−x,x=0,1 The expected...","categories": ["Mathematical Statistics"],
+        "tags": ["statistics"],
+        "url": "/math_stat/ch3/",
+        "teaser": null
+      },{
+        "title": "[Mathematical Statistics] Chapter3",
+        "excerpt":"Bernoulli and Binomial Bernoulli Distribution Bernoulli experiment a random experiment that outcome are classified with two mutually exclusive and exhaustive ways Bernoulli process a sequence of Bernoulli trials. Let X be a random variable associated with a Bernoulli trial The pmf of X is p(x)=px(1−p)1−x,x=0,1p(x) = p^x(1-p)^{1-x}, x=0,1p(x)=px(1−p)1−x,x=0,1 The expected...","categories": ["math_stat","Mathematical Statistics"],
+        "tags": ["statistics"],
+        "url": "/math_stat/ch3-1/",
+        "teaser": null
+      },{
+        "title": "Questions in Python",
+        "excerpt":"General Python          How class and get works https://stackoverflow.com/a/44741455            Adjusting tqdm message with sys.stdout and sys.stderr modification https://stackoverflow.com/questions/36986929/redirect-print-command-in-python-script-through-tqdm-write/37243211#37243211       Pandas          Make DataFrame iterable based on index (row-wise iteration) https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas            How to use MultiIndex? Advanced pandas https://towardsdatascience.com/how-to-use-multiindex-in-pandas-to-level-up-your-analysis-aeac7f451fce       Numpy     Hadamard product https://stackoverflow.com/questions/40034993/how-to-get-element-wise-matrix-multiplication-hadamard-product-in-numpy  ","categories": ["Python","Pandas","Numpy"],
+        "tags": ["Python"],
+        "url": "/python/ask-python/",
+        "teaser": null
+      },{
+        "title": "Time Series 1",
+        "excerpt":"multiple h1 is availalbe? White noise We assume it is uncorrelated Cov(at,at+1)=0Cov(a_t, a_{t+1}) = 0Cov(at​,at+1​)=0 Covariance measures linear relationship between X, Y. e.g.) Y=X2+1Y=X^2+1Y=X2+1 have Cov=0Cov=0Cov=0 e.g.2) What if Y=X2Y=X^2Y=X2? Cov(X,Y)=E[X⋅X2]−E[x]E[X2]Cov(X,Y) = E[X \\cdot X^2] - E[x]E[X^2]Cov(X,Y)=E[X⋅X2]−E[x]E[X2]. Since X and Y are (0,0) symmetric, Cov(X,Y)=0Cov(X,Y) = 0Cov(X,Y)=0 E(at)=0E(a_t) = 0E(at​)=0...","categories": ["Time Series"],
+        "tags": ["statistics"],
+        "url": "/time-series/",
+        "teaser": null
+      }]
