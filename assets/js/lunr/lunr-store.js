64893cb7be5f361308f46693a1b64cf17393aa52
leeway00 <leeway00@users.noreply.github.com>
2022-04-25 07:32:44 +0000

    deploy: 5121418ec344b25116d300e60b4ba442f82611e7

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,10 +1,4 @@
 var store = [{
-        "title": "Quick-Start Guide",
-        "excerpt":"Minimal Mistakes has been developed as a Gem-based theme for easier use, and 100% compatible with GitHub Pages when used as a remote theme. If you enjoy this theme, please consider sponsoring me to continue developing and maintaining it. Installing the theme If you’re running Jekyll v3.7+ and self-hosting you...","categories": [],
-        "tags": [],
-        "url": "/data_science/test/",
-        "teaser": null
-      },{
         "title": "Chapter3 Special Distributions",
         "excerpt":"Bernoulli and Binomial Bernoulli Distribution Bernoulli experiment a random experiment that outcome are classified with two mutually exclusive and exhaustive ways Bernoulli process a sequence of Bernoulli trials. Let X be a random variable associated with a Bernoulli trial The pmf of X is p(x)=px(1−p)1−x,x=0,1p(x) = p^x(1-p)^{1-x}, x=0,1p(x)=px(1−p)1−x,x=0,1 The expected...","categories": ["Mathematical Statistics"],
         "tags": ["statistics"],
@@ -26,6 +20,18 @@ var store = [{
         "title": "Time Series 1",
         "excerpt":"multiple h1 is availalbe? White noise We assume it is uncorrelated Cov(at,at+1)=0Cov(a_t, a_{t+1}) = 0Cov(at​,at+1​)=0 Covariance measures linear relationship between X, Y. e.g.) Y=X2+1Y=X^2+1Y=X2+1 have Cov=0Cov=0Cov=0 e.g.2) What if Y=X2Y=X^2Y=X2? Cov(X,Y)=E[X⋅X2]−E[x]E[X2]Cov(X,Y) = E[X \\cdot X^2] - E[x]E[X^2]Cov(X,Y)=E[X⋅X2]−E[x]E[X2]. Since X and Y are (0,0) symmetric, Cov(X,Y)=0Cov(X,Y) = 0Cov(X,Y)=0 E(at)=0E(a_t) = 0E(at​)=0...","categories": ["Time Series"],
         "tags": ["statistics"],
-        "url": "/time-series/",
+        "url": "/time-series/ts1/",
+        "teaser": null
+      },{
+        "title": "Time Series 1",
+        "excerpt":"multiple h1 is availalbe? White noise We assume it is uncorrelated Cov(at,at+1)=0Cov(a_t, a_{t+1}) = 0Cov(at​,at+1​)=0 Covariance measures linear relationship between X, Y. e.g.) Y=X2+1Y=X^2+1Y=X2+1 have Cov=0Cov=0Cov=0 e.g.2) What if Y=X2Y=X^2Y=X2? Cov(X,Y)=E[X⋅X2]−E[x]E[X2]Cov(X,Y) = E[X \\cdot X^2] - E[x]E[X^2]Cov(X,Y)=E[X⋅X2]−E[x]E[X2]. Since X and Y are (0,0) symmetric, Cov(X,Y)=0Cov(X,Y) = 0Cov(X,Y)=0 E(at)=0E(a_t) = 0E(at​)=0...","categories": ["Time Series"],
+        "tags": ["statistics"],
+        "url": "/time_series/ts1/",
+        "teaser": null
+      },{
+        "title": "Quick-Start Guide",
+        "excerpt":"Minimal Mistakes has been developed as a Gem-based theme for easier use, and 100% compatible with GitHub Pages when used as a remote theme. If you enjoy this theme, please consider sponsoring me to continue developing and maintaining it. Installing the theme If you’re running Jekyll v3.7+ and self-hosting you...","categories": [],
+        "tags": [],
+        "url": "/time_series/test/",
         "teaser": null
       }]
